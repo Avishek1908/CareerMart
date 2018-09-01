6eb4a29a949edd99f1cc308122cf1c831d6a1a7f
bhargav0528 <bhargavauden1998@gmail.com>
2018-09-02 02:34:33 +0530

    Donnee

--- app/src/main/java/com/dsce/dbms/careermart/HomeActivity.java
+++ app/src/main/java/com/dsce/dbms/careermart/HomeActivity.java
@@ -22,7 +22,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_activity);
 
         tabs = (TabLayout)findViewById(R.id.tabs);
         tabs.addTab(tabs.newTab().setText("Home"));
